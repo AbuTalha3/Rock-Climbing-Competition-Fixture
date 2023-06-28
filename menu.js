0d191365be2c6d719b5a0602311dd88f4d3379c3
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-06-28 15:37:21 +0500

    Update Javascript file

---
+++ menu.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector(".heading-menu");
+const closeicons = document.querySelector(".close");
+const hamburger = document.querySelector(".menubars");
+
+const showMenu = () => {
+  hamburger.style.display = "block";
+  closeicons.style.transform = "translateY(0)";
+  menu.style.transform = "translateY(0)";
+};
+
+const hideMenu = () => {
+  closeicons.style.transform = "translateY(-20rem)";
+  menu.style.transform = "translateY(-200%)";
+};
+
+hamburger.addEventListener("click", showMenu);
+closeicons.addEventListener("click", hideMenu);
